8d2c97b6c22bd2b45ad662d56affaba25ec86055
Axel Björnfot <axelbjornfot@gmail.com>
2021-02-16 14:36:16 +0100

    Buttons

---
+++ src/components/ButtonsContainer.js
@@ -0,0 +1,11 @@
+import { buttons } from "../css/ButtonsContainer.module.css"
+
+function ButtonsContainer () {
+return (
+    <div className={buttons}>
+        <p>Here will map buttons</p>
+    </div>
+)
+}
+
+export default ButtonsContainer;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,15 @@
 import Display from "../components/Display"
+import ButtonsContainer from "./ButtonsContainer";
 import { calculator } from "../css/Calculator.module.css";
+
+
 
 function Calculator () {
     return (
         <div className={calculator}>
 <Display />
+<ButtonsContainer />
+
         </div>
     )
 }
